0b47d30ce56d9cdc1183c2a202d766e96b6116f3
niuiic <niuiicde@gmail.com>
2023-10-20 10:32:34 +0800

    refactor: improve code

--- plugin/typst-preview.lua
+++ plugin/typst-preview.lua
@@ -2,9 +2,13 @@ local static = require("typst-preview.static")
 local utils = require("typst-preview.utils")
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "*" },
+	pattern = { "*.typ" },
 	callback = function(args)
 		utils.redirect_pdf(args.buf)
+
+		if static.config.clean_temp_pdf then
+			utils.collect_temp_pdf(args.buf)
+		end
 	end,
 })
 
@@ -12,14 +16,5 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
 	pattern = { "*" },
 	callback = function()
 		utils.clean_pdf()
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "*" },
-	callback = function(args)
-		if static.config.clean_temp_pdf then
-			utils.collect_temp_pdf(args.buf)
-		end
 	end,
 })
